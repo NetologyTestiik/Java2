e99fda1b0026de7956310142765e9995bf8c417b
Neto test <veronikakaravanova60@gmail.com>
2025-02-27 15:01:21 +0300

    change README

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amout = 14000;
+        int mile = 20;
+        int bonus = amout / mile;
+
+        System.out.println("Итоговый бонус:" + bonus);
+    }
+
+}
